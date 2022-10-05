ea12a08fc2952c9418c2d33969f23d83e7b0ff20
yokohama <yuhei.yokohama@gmail.com>
2022-10-05 12:30:13 +0900

    デプロイテスト

--- ops/regist_rails_master_key.sh
+++ ops/regist_rails_master_key.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 MASTER_KEY=`cat config/master.key`
-echo $MASTER_KEY
-#aws secretsmanager update-secret --secret-id rails-master-key-prod --secret-string ${MASTER_KEY}
+aws secretsmanager update-secret --secret-id rails-master-key-prod --secret-string {\"railsMasterKey\":\"${MASTER_KEY}\"}
